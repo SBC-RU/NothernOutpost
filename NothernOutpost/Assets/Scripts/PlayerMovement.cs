295035a7f254786dc7b21fb18ee092b81eadb4ab
SBC-RU <114054851+SBC-RU@users.noreply.github.com>
2024-03-11 15:07:22 +0300

    Перемещение и прижок
    Добавил перемещение и прыжок

---
+++ NothernOutpost/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public CharacterController controller;
+
+    public float speed = 12f;
+    public float gravity = -9.81f; //коэфициент гравитации
+    public float jumpHeight = 3f;
+    public Transform groundCheck;
+    public float groundDistance=0.4f;
+    public LayerMask groundMask;
+    Vector3 velocity;
+    bool isGrounded; //хранит значение коснулся земли или нет
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask); //создание сферы для проверки приземления
+        if(isGrounded && velocity.y < 0) //проверка на приземление
+        {
+            velocity.y = -3f;
+        }
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+        Vector3 move = transform.right * x + transform.forward * z;
+        controller.Move(move * speed * Time.deltaTime);
+
+        if(Input.GetButtonDown("Jump") && isGrounded)
+        {
+            velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
+        }
+        velocity.y += gravity * Time.deltaTime;
+        controller.Move(velocity * Time.deltaTime);
+    }
+}
